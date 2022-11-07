5d8de8adf14f80f36563553cbed8cf29cc43184d
engrkashem <kashemaust@gmail.com>
2022-11-07 14:39:52 +0600

    extract usd rate from currency rate file and make airport with extracted usd rate for correnponding country

--- airport.py
+++ airport.py
@@ -1,13 +1,13 @@
 
 class Airport:
-    def __init__(self, code, name, city, country, lat, long, fare) -> None:
+    def __init__(self, code, name, city, country, lat, long, usd_rate) -> None:
         self.code = code
         self.port_name = name
         self.city = city
         self.country = country
         self.lat = lat
         self.long = long
-        self.fare = fare
+        self.usd_rate = usd_rate
 
     def __repr__(self) -> str:
-        return f'Airport {self.port_name} in {self.country}, Latitude: {self.lat}, Longitude: {self.long} and fare: {self.fare}'
+        return f'Airport {self.port_name} in {self.country}, Latitude: {self.lat}, Longitude: {self.long} and USD_Rate: {self.usd_rate}'
